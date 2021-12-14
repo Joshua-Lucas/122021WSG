194adc43c9323db81bdf0e1e2219bf7a2c3a6776
joshua-lucas <joshuamlucas91@gmail.com>
2021-12-14 01:18:47 -0500

    Add design system assets and globel styles

---
+++ frontend/src/assets/designSystem/Theme.js
@@ -0,0 +1,9 @@
+export const Theme = {
+  colors: {
+    primary: '#FFF',
+    secondary: 'rgb(1, 1, 20)',
+    darkAccent: '#222a96',
+    lightAccent: 'rgb(0, 180, 216)',
+    neutral: 'rgb(202, 240, 248)',
+  },
+};
