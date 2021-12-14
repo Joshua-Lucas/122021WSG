b039f01e0d10733b2036d892a8ffa5e0ba1f429a
joshua-lucas <joshuamlucas91@gmail.com>
2021-12-13 22:17:22 -0500

    Setup frontend evniroment

---
+++ frontend/src/components/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { useState } from 'react';
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="background">
+      <p>{count}</p>
+      <button onClick={() => setCount(count + 1)}>Increment</button>
+      <button onClick={() => setCount(count - 1)}>Decrement</button>
+    </div>
+  );
+}
+
+export default App;
